0197ecdd69523c560077b9b5c28a0cbe38407920
Sam C <hoche002@gmail.com>
2020-11-15 02:03:45 +0100

    moved packages to root

--- home.nix
+++ home.nix
@@ -1,31 +1,38 @@
 { config, pkgs, ... }:
 
 {
-  programs.home-manager.enable = true;
+  programs.home-manager = {
+	  enable = true;
+	};
 
-  home.username = "samcheung";
-  home.homeDirectory = "/home/samcheung";
+  home = {
+	  username = "samcheung";
+    homeDirectory = "/home/samcheung";
+	};
+	
 	nixpkgs.config.allowUnfree = true;
 	
   home.packages = with pkgs; [
-				git
-				qutebrowser
 				rclone
 				discord
 				mupdf
-				keepassxc
-				vim
 				cmake
 				libtool
 				libvterm
 				qbittorrent
-				emacsPackages.vterm
 				imagemagick
 				xclip
 				texlive.combined.scheme-medium
-				light
 				hunspell
 				hunspellDicts.en-us
+				gnuplot
+				krita
+				audacity
+				vlc
+				ripgrep
+				dnsmasq
+				sqlite
+				emacsPackages.vterm
+				emacsPackages.pdf-tools
 	];
-
 }
